55fb7f4fd0dc3db7570462f0227ad80beee48c09
Diego Barrera <diego.barreramartinez@bdr.de>
2022-11-11 15:40:29 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Map from "./lib/Map.svelte";
+</script>
+
+<main>
+  <Map />
+</main>
+
+<style>
+</style>
---
+++ src/lib/Map.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="map" />
+
+<style>
+  .map {
+    width: 100vw;
+    height: 100vh;
+    inset: 0;
+    background-image: url("/public/static/map.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
